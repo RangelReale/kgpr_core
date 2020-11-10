9386452b5b500c198873176d7bd1304a856a3c62
Rangel Reale <rangelspam@gmail.com>
2020-11-10 15:52:01 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setuptools.setup(
     name="kgpr_core",
     version=__version__,
     author="Rangel Reale",
-    author_email="rangelspam@gmail.com",
+    author_email="rangelreale@gmail.com",
     description="KubraGen Providers: Core",
     long_description=long_description,
     long_description_content_type="text/markdown",
